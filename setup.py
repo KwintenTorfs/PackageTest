167fe51a9106d690b8a9973b47c881909989d628
KwintenTorfs <kwinten.torfs@kuleuven.be>
2024-01-03 12:28:22 +0100

    sdf

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='packageOne',
+      version='0.0.1',
+      description='Test Package',
+      author='Kwinten Torfs',
+      author_email='torfs.kwinten@gmail.com',
+      url='https://github.com/KwintenTorfs/PackageTest',
+      packages=['packageOne'],
+      license='KUL',
+      )
