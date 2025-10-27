f08ddc1c2a24cd6937652b6bd1ed1497cfecccf8
ktorfs <kwinten.torfs@kuleuven.be>
2025-10-27 14:52:49 +0100

    Area Rectangle

--- packageOne/area.py
+++ packageOne/area.py
@@ -5,6 +5,12 @@ def area_rectangle(width, height):
     area = width * height
     return area
 
+
 def area_circle(radius):
     area = math.pi * radius**2
+    return area
+
+
+def area_triangle(width, height):
+    area = width * height / 2
     return area
